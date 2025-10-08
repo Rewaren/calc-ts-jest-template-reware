e28677d5002401f579ef23276926a142a33c8dfc
warenr02 <warenr02@gmail.com>
2025-10-08 13:36:45 +0300

    Part 1. functions 3, 4 and 5

--- src/HW05.ts
+++ src/HW05.ts
@@ -1,11 +1,28 @@
-function multiply(a: number, b: number): number {
+export function multiply(a: number, b: number): number {
     return (a*b);
 }
 
-function calculatePercentage(part: number, total: number): string {
+export function calculatePercentage(part: number, total: number): string {
     const percentage = (part / total) * 100;
     const formatted = `${percentage.toFixed(2)}%`;
     return formatted;
 }
 
-console.log(calculatePercentage(2, 10))
+export function isEven(num: number): boolean {
+    return num % 2 === 0;
+}
+
+export function compareStrings(text1: string, text2: string): boolean{
+    return text1 === text2;
+}
+
+export function isPositive(num: number): boolean{
+    return num > 0;
+}
+
+console.log(calculatePercentage(2, 10)) // 20.00%
+console.log(isEven(5)) // false
+console.log(compareStrings("apple", "apple")); // true
+console.log(compareStrings("apple", "banana")); // false
+console.log(isPositive(-1)) // false
+console.log(isPositive(5)) // true
