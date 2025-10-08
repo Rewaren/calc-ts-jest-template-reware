703055a9b23bd158b097e37633cda0f7add13400
warenr02 <warenr02@gmail.com>
2025-10-08 17:35:24 +0300

    all tests ok

--- src/HW05.ts
+++ src/HW05.ts
@@ -3,8 +3,11 @@ export function multiply(a: number, b: number): number {
 }
 
 export function calculatePercentage(part: number, total: number): string {
-    const percentage = (part / total) * 100;
-    return `${percentage.toFixed(2)}%`;
+    if (total > 0 && part > 0) {
+        const percentage = (part / total) * 100;
+        return `${percentage.toFixed(2)}%`;
+    }
+    return "Input contains negative number."
 
 }
 
@@ -17,7 +20,7 @@ export function compareStrings(text1: string, text2: string): boolean{
 }
 
 export function isPositive(num: number): boolean{
-    return num > 0;
+    return num >= 0;
 }
 
 console.log(calculatePercentage(2, 10)) // 20.00%
--- test/hw05.test.ts
+++ test/hw05.test.ts
@@ -2,42 +2,59 @@ import {multiply, calculatePercentage, isEven, compareStrings, isPositive} from 
 
 describe('HW05 Functions testing', () => {
 
-    test('multiply two integer positive numbers', () => {
+    // Test multiply
+    test('multiply two positive integer numbers', () => {
         expect(multiply(2, 3)).toBe(6)
     })
-    test('multiply one integer positive and one negative number', () => {
+    test('multiply one positive integer and one negative integer number', () => {
         expect(multiply(2, -3)).toBe(-6)
     })
+    test('multiply two negative integer numbers', () => {
+        expect(multiply(-2, -3)).toBe(6)
+    })
 
+    //Test calculatePercentage
     test('calculate percentage when one number is part of another', () => {
         expect(calculatePercentage(2, 10)).toBe("20.00%")
     })
-    //test('calculate percentage when one number is negative', () => {
-    //    expect(calculatePercentage(-2, 10)).toBe("20.00%") // !!!!
-    //})
-    //test('calculate percentage when both numbers are negative', () => {
-    //    expect(calculatePercentage(-2, 10)).toBe("20.00%") // !!!!
-    //})
+    test('calculate percentage when one number is negative', () => {
+        expect(calculatePercentage(-2, 10)).toBe("Input contains negative number.")
+    })
+    test('calculate percentage when both numbers are negative', () => {
+        expect(calculatePercentage(-2, -10)).toBe("Input contains negative number.")
+    })
 
-    test('return true for positive even integer number', () => {
+    // Test isEven
+    test('return true for positive even number', () => {
         expect(isEven(2)).toBeTruthy()
     })
-    test('return false for positive not even integer number', () => {
+    test('return false for positive not even number', () => {
         expect(isEven(5)).toBeFalsy()
     })
-    test('return false for negative not even integer number', () => {
+    test('return false for negative not even number', () => {
         expect(isEven(-1)).toBeFalsy()
     })
 
+    // Test compareStrings
     test('return true for two same strings', () => {
         expect(compareStrings("apple", "apple")).toBeTruthy()
     })
     test('return false for same strings, but one is shouting', () => {
         expect(compareStrings("apple", "APPLE")).toBeFalsy()
     })
+    test('return false for different strings', () => {
+        expect(compareStrings("apple", "banana")).toBeFalsy()
+    })
 
+    // Test isPositive
     test('return true for positive integer number', () => {
         expect(isPositive(5)).toBeTruthy()
+    })
+    test('return false for negative integer number', () => {
+        expect(isPositive(-1)).toBeTruthy()
+    })
+    test('return true for zero', () => {
+        expect(isPositive(0)).toBeTruthy()
     })
 
 })
