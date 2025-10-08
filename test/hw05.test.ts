ac9250c7ad2fb8f49dc187fc076897e938707889
warenr02 <warenr02@gmail.com>
2025-10-08 16:07:41 +0300

    tests for functions isEven, calculatePercentage, multiply

--- test/hw05.test.ts
+++ test/hw05.test.ts
@@ -1,16 +1,39 @@
-import {MathFunctions} from '../src/HW05'
+import {multiply, calculatePercentage, isEven, compareStrings, isPositive} from '../src/HW05'
 
-describe('HW05 Functions Class testing', () => {
-    let func: MathFunctions
+describe('HW05 Functions testing', () => {
 
-    test('should add two numbers correctly', () => {
-        func = new MathFunctions()
-        expect(func.multiply(2, 3)).toBe(6)
+    test('multiply two integer positive numbers', () => {
+        expect(multiply(2, 3)).toBe(6)
+    })
+    test('multiply one integer positive and one negative number', () => {
+        expect(multiply(2, -3)).toBe(-6)
     })
 
-    test('should add two numbers correctly', () => {
-        func = new MathFunctions()
-        expect(func.calculatePercentage(2, 10)).toBe("20.00%")
+    test('calculate percentage when one number is part of another', () => {
+        expect(calculatePercentage(2, 10)).toBe("20.00%")
+    })
+    //test('calculate percentage when one number is negative', () => {
+    //    expect(calculatePercentage(-2, 10)).toBe("20.00%") // !!!!
+    //})
+    //test('calculate percentage when both numbers are negative', () => {
+    //    expect(calculatePercentage(-2, 10)).toBe("20.00%") // !!!!
+    //})
+
+    test('return true for positive even integer number', () => {
+        expect(isEven(2)).toBeTruthy()
+    })
+    test('return false for positive not even integer number', () => {
+        expect(isEven(5)).toBeFalsy()
+    })
+    test('return false for negative not even integer number', () => {
+        expect(isEven(-1)).toBeFalsy()
+    })
+
+    test('return true for two same strings', () => {
+        expect(compareStrings("apple", "apple")).toBeTruthy()
+    })
+    test('return false for same strings, but one is shouting', () => {
+        expect(compareStrings("apple", "APPLE")).toBeFalsy()
     })
 
 })
